90a39af36f154af16d971ad92a72918eb11a985e
aexol <aexol@aexol.com>
2018-03-30 17:31:57 +0200

    removed array transformations

--- src/syncano/formMappings/index.tsx
+++ src/syncano/formMappings/index.tsx
@@ -25,21 +25,6 @@ export const syncanoRelations = ({ fields, models }) => {
 export const syncanoReceive = ({ fields, models, values }) => {
   let newValues = { ...values };
   fields.map((f) => {
-    if (['relation', 'reference'].indexOf(f.type) !== -1) {
-      let ref = models.find((r) => r.name === f.target);
-      if (values[f.name] && f.type === 'reference') {
-        newValues[f.name] = ref.objects.find((r) => r.id === values[f.name])[ref.display];
-      }
-      if (values[f.name] && f.type === 'relation') {
-        newValues[f.name] = values[f.name].map((v) => ({
-          label: ref.objects.find((r) => r.id === v)[ref.display],
-          value: v
-        }));
-      }
-    }
-    if (f.type === 'array' && values[f.name]) {
-      newValues[f.name] = values[f.name].map((v) => ({ label: v, value: v }));
-    }
     if (f.type === 'datetime' && values[f.name]) {
       newValues[f.name] = moment(values[f.name]);
     }
@@ -49,15 +34,6 @@ export const syncanoReceive = ({ fields, models, values }) => {
 export const syncanoSend = ({ fields, values }) => {
   let newValues = { ...values };
   fields.map((f) => {
-    if (f.type === 'array' && values[f.name]) {
-      newValues[f.name] = values[f.name].map((v) => v.value);
-    }
-    if (f.type === 'relation' && values[f.name]) {
-      newValues[f.name] = values[f.name].map((v) => v.value);
-    }
-    if (f.type === 'reference' && values[f.name]) {
-      newValues[f.name] = values[f.name].value;
-    }
     if (f.type === 'datetime' && values[f.name]) {
       newValues[f.name] = values[f.name].toISOString();
     }
